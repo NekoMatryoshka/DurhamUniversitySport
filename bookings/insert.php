78b18f89bc145fe257ca2c447ddc152f100624f9
Won Jun Youn <kedus1234@gmail.com>
2019-05-08 13:50:39 +0000

    insert.php edited online with Bitbucket

--- bookings/insert.php
+++ bookings/insert.php
@@ -4,20 +4,15 @@
 
 require '../DB/DB_Connection.php';
 
-if(isset($_POST["name"]))
+if(isset($_POST["m_id"]))
 {
- 
- //$f_id = explode('|',$_POST['f_id']);
- 
  $query = "
  INSERT INTO bookings 
- (name, f_id, f_name, start_time, end_time) 
- VALUES ('".$_POST["name"]."', '".$_POST["f_id"]."', '".$_POST["f_name"]."', '".$_POST["start_time"]."', '".$_POST["end_time"]."')
+ (m_id, m_name, f_id, f_name, start_time, end_time) 
+ VALUES ('".$_POST["m_id"]."', '".$_POST["m_name"]."', '".$_POST["f_id"]."', '".$_POST["f_name"]."', '".$_POST["start_time"]."', '".$_POST["end_time"]."')
  ";
  $statement = $connect->prepare($query);
  $statement->execute();
 }
-
-
 
 ?>
