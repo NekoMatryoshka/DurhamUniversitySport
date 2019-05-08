59de7c05c212086f12b592090697f3adedf52ba9
Won Jun Youn <kedus1234@gmail.com>
2019-05-08 12:46:48 +0000

    dropdown.php created online with Bitbucket

---
+++ bookings/dropdown.php
@@ -0,0 +1,25 @@
+<?php
+	require '../DB/DB_Connection.php';
+
+	$query = "SELECT id, name FROM facilities";
+	$statement = $connect->prepare($query);
+	$statement->execute();
+	$result = $statement->fetchAll();
+	$total_row = $statement->rowCount();
+	$output ='<select id="facility" name="facility" class="form-control">
+				<option value="all">All</option>';
+	if($total_row > 0)
+	{
+		foreach($result as $row)
+		{
+			//$output .= '<option value="'.$row["id"].'|'.$row["name"].'">'.$row["name"].'</option>';
+			$output .= '<option value="'.$row["id"].'">'.$row["name"].'</option>';
+		}
+	}
+	$output.='</select>';
+	echo $output;
+?>
+
+
+
+			
