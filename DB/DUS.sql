108e71aa915032c3e20815917905ff32f2582bc0
Won Jun Youn <kedus1234@gmail.com>
2019-05-08 13:52:03 +0000

    DUS.sql edited online with Bitbucket

--- DB/DUS.sql
+++ DB/DUS.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- 생성 시간: 19-05-08 11:14
+-- 생성 시간: 19-05-08 15:51
 -- 서버 버전: 10.1.38-MariaDB
 -- PHP 버전: 7.3.2
 
@@ -32,7 +32,8 @@ CREATE TABLE `bookings` (
   `id` int(10) NOT NULL,
   `f_id` int(10) NOT NULL,
   `f_name` varchar(30) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `m_id` int(30) NOT NULL,
+  `m_name` varchar(50) NOT NULL,
   `start_time` datetime NOT NULL,
   `end_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -41,32 +42,12 @@ CREATE TABLE `bookings` (
 -- 테이블의 덤프 데이터 `bookings`
 --
 
-INSERT INTO `bookings` (`id`, `f_id`, `f_name`, `name`, `start_time`, `end_time`) VALUES
-(49, 82, 'Fancing Salle', 'luis', '2019-05-16 11:00:00', '2019-05-16 14:00:00'),
-(50, 82, 'Fancing Salle', 'luis', '2019-05-10 14:00:00', '2019-05-10 18:00:00'),
-(51, 106, 'asdasd', 'luis', '2019-05-14 11:00:00', '2019-05-14 15:00:00'),
-(52, 77, 'Squash Court', 'luis', '2019-05-21 12:00:00', '2019-05-21 18:00:00'),
-(53, 77, 'Squash Court', 'luis', '2019-05-10 15:00:00', '2019-05-10 16:00:00'),
-(54, 77, 'Squash Court', 'luis', '2019-05-30 13:00:00', '2019-05-30 14:00:00'),
-(55, 77, 'Squash Court', 'luis', '2019-05-12 16:00:00', '2019-05-12 18:00:00'),
-(56, 80, 'Sports Hall', 'luis', '2019-05-07 13:00:00', '2019-05-07 14:00:00'),
-(57, 80, 'Sports Hall', 'luis', '2019-05-30 09:00:00', '2019-05-30 11:00:00'),
-(58, 80, 'Sports Hall', 'luis', '2019-06-01 12:00:00', '2019-06-01 16:00:00'),
-(59, 82, 'Fancing Salle', 'luis', '2019-05-26 10:00:00', '2019-05-26 18:00:00'),
-(60, 82, 'Fancing Salle', 'luis', '2019-05-02 11:00:00', '2019-05-02 15:00:00'),
-(61, 82, 'Fancing Salle', 'luis', '2019-04-30 09:00:00', '2019-04-30 15:00:00'),
-(62, 106, 'asdasd', 'luis', '2019-05-10 10:00:00', '2019-05-10 18:00:00'),
-(68, 106, 'Tennis', 'luis', '2019-05-23 11:00:00', '2019-05-23 14:00:00'),
-(69, 110, 'Foot ball ', 'luis', '2019-05-21 11:00:00', '2019-05-21 15:00:00'),
-(70, 110, 'Foot ball ', 'luis', '2019-05-24 14:00:00', '2019-05-24 15:00:00'),
-(71, 77, 'Squash Court', 'Yating', '2019-05-15 10:00:00', '2019-05-15 13:00:00'),
-(72, 77, 'Squash Court', 'Yating', '2019-05-17 11:00:00', '2019-05-17 16:00:00'),
-(73, 77, 'Squash Court', 'Yating', '2019-05-27 15:00:00', '2019-05-27 18:00:00'),
-(74, 106, 'Tennis', 'Yating', '2019-05-20 09:00:00', '2019-05-20 13:00:00'),
-(75, 106, 'Tennis', 'Yating', '2019-05-30 13:00:00', '2019-05-30 17:00:00'),
-(76, 110, 'Foot ball ', 'Yating', '2019-05-05 14:00:00', '2019-05-05 18:00:00'),
-(77, 110, 'Foot ball ', 'Yating', '2019-05-03 15:00:00', '2019-05-03 18:00:00'),
-(80, 80, 'Sports Hall', 'luis', '2019-05-21 11:00:00', '2019-05-21 15:00:00');
+INSERT INTO `bookings` (`id`, `f_id`, `f_name`, `m_id`, `m_name`, `start_time`, `end_time`) VALUES
+(87, 106, 'Tennis', 10, 'luis', '2019-05-06 10:00:00', '2019-05-06 14:00:00'),
+(88, 106, 'Tennis', 10, 'luis', '2019-05-13 11:00:00', '2019-05-13 12:00:00'),
+(89, 110, 'Foot ball ', 10, 'luis', '2019-05-21 09:00:00', '2019-05-21 16:00:00'),
+(91, 110, 'Foot ball ', 11, 'Yating', '2019-05-04 10:00:00', '2019-05-04 17:00:00'),
+(92, 110, 'Foot ball ', 11, 'Yating', '2019-05-03 11:00:00', '2019-05-03 13:00:00');
 
 -- --------------------------------------------------------
 
@@ -120,7 +101,9 @@ CREATE TABLE `members` (
 INSERT INTO `members` (`id`, `m_id`, `password`, `name`, `email`, `tel`, `type`) VALUES
 (6, '121212', '1212', '1231212', '1212', '12355', 'user'),
 (10, 'luis', '54321', 'WON J Y', 'kedus1234@gmail.com', '1222352342', 'admin'),
-(11, 'Yating', '0726', 'Liu Yating', '213', '312213', 'user');
+(11, 'Yating', '0726', 'Liu Yating', '213', '312213', 'user'),
+(13, 'qwqwd', 'wqdqwd', 'qwdwq', 'qwdqwd', 'qwdqwd', 'user'),
+(14, 'mark', 'goodguy', 'rick', 'fkluis@assasin.com', 'come to my room', 'user');
 
 --
 -- 덤프된 테이블의 인덱스
@@ -131,7 +114,8 @@ INSERT INTO `members` (`id`, `m_id`, `password`, `name`, `email`, `tel`, `type`)
 --
 ALTER TABLE `bookings`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `f_id` (`f_id`);
+  ADD KEY `f_id` (`f_id`),
+  ADD KEY `bookings_ibfk_2` (`m_id`);
 
 --
 -- 테이블의 인덱스 `facilities`
@@ -153,7 +137,7 @@ ALTER TABLE `members`
 -- 테이블의 AUTO_INCREMENT `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 
 --
 -- 테이블의 AUTO_INCREMENT `facilities`
@@ -165,7 +149,7 @@ ALTER TABLE `facilities`
 -- 테이블의 AUTO_INCREMENT `members`
 --
 ALTER TABLE `members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- 덤프된 테이블의 제약사항
@@ -175,7 +159,8 @@ ALTER TABLE `members`
 -- 테이블의 제약사항 `bookings`
 --
 ALTER TABLE `bookings`
-  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`f_id`) REFERENCES `facilities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`f_id`) REFERENCES `facilities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`m_id`) REFERENCES `members` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
