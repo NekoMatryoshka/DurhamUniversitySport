88d83589dc80dc9d205b57754d4d2f68798ec6cf
Won Jun Youn <kedus1234@gmail.com>
2019-05-15 10:02:22 +0000

    load.php edited online with Bitbucket

--- bookings/load.php
+++ bookings/load.php
@@ -1,88 +1,25 @@
 <?php
-include("../DB/DB_Connection.php");
 
+require '../DB/DB_Connection.php';
 
-if($_POST["session_type"] == "admin"){
-	if(isset($_POST["query"]))
-	{	
-		$search = $_POST["query"];
-		$query = "
-		SELECT * FROM members 
-		WHERE m_id LIKE '%".$search."%'
-		OR email LIKE '%".$search."%'
-		OR tel LIKE '%".$search."%'
-		OR name LIKE '%".$search."%'
-		";
-	}
-	else
-	{
-		$query = "SELECT * FROM members";	
-	}
-}
-else
-{
-	$query = "SELECT * FROM members WHERE id = '".$_POST["session_id"]."'
-	";		
-}
+$query = "SELECT * FROM bookings";
 
+$data = array();
 $statement = $connect->prepare($query);
 $statement->execute();
 $result = $statement->fetchAll();
-$total_row = $statement->rowCount();
-$output = '
-	<table class="table table-striped table-dark text-center">
-		<tr>
-			<th>ID</th>
-			<th>Name</th>
-			<th>Password</th>
-			<th>Email</th>
-			<th>Tel</th>
-			<th>Action</th>
-		</tr>
-	';
-		
-if($total_row > 0)
+foreach($result as $row)
 {
-	foreach($result as $row)
-	{
-		
-		
-		$output .='
-		<tr>
-			<td width="15%">'.$row["m_id"].'</td>
-			<td width="20%">'.$row["name"].'</td>
-			<td width="10%"> *Secret Info* </td>
-			<td width="20%">'.$row["email"].'</td>
-			<td width="15%">'.$row["tel"].'</td>
-			<td width="20%">';
-				if($_POST["session_type"] == "admin")
-				{
-				$output .='
-				<button type="button" name="edit" class="btn btn-outline-primary edit" id="'.$row["id"].'">Edit</button>
-				<button type="button" name="delete" class="btn btn-outline-danger delete" id="'.$row["id"].'">Del</button>
-				';
-				}
-				else
-				{
-				$output .='
-				<button type="button" name="edit" class="btn btn-outline-primary edit" id="'.$row["id"].'">Edit</button>
-				<button type="button" name="delete" class="btn btn-outline-danger delete" id="'.$row["id"].'">Del</button>
-				';
-				}
-			$output.='		
-			</td>
-		</tr>	
-		';
-	}
+	$data[] = array(
+	'id' => $row["id"],
+	'f_id' => $row["f_id"],
+	'title' => $row["m_name"],
+	'start' => $row["start_time"],
+	'end' => $row["end_time"]
+	);
+	
 }
-else
-{
-	$output .='
-	<tr>
-		<td colspan="8" align="center">Data not found</td>
-	</tr>
-	';
-}
-$output .= '</table>';
-echo $output;
+
+echo json_encode($data);
+
 ?>
